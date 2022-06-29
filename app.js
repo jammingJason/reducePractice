1b2be986a1068536c5f05431005cd0eab0498f51
Van Jason Crews <vanjasoncrews@gmail.com>
2022-06-29 16:21:42 -0400

    modified files

--- app.js
+++ app.js
@@ -205,24 +205,25 @@
 const nums = [ 20, 30, 50, 12, -2, 45, 99, 19, 22, 85 ];
 
 let total = 0;
-for (let num of nums) {
-	total += num;
+for (const num of nums) {
+	total = total + num;
 }
 console.log(total);
 
 let min = nums[0];
 for (let i = 1; i < nums.length; i++) {
-	if (nums[i] < min) min = nums[i];
+	if (min > nums[i]) {
+		min = nums[i];
+	}
 }
 console.log(min);
 
-const str = 'lollapalooza';
+let str = 'lollapalooza';
 const charFreq = {};
 for (let char of str) {
 	if (charFreq[char]) {
 		charFreq[char] += 1;
-	}
-	else {
+	} else {
 		charFreq[char] = 1;
 	}
 }
@@ -233,25 +234,15 @@ const result = words.reduce(function(accum, nextWord) {
 	return accum + nextWord;
 });
 
-const midtermScores = [ 70, 88, 93, 94, 64, 62, 56 ];
-const finalsScores = [ 92, 93, 76, 77, 78, 59, 61 ];
-// const minScore = midtermScores.reduce(function(min, nextScore) {
-// 	if (nextScore < min) {
-// 		return nextScore;
-// 	}
-// 	return min;
-// });
-
-const minMidtermScore = midtermScores.reduce(function(min, nextScore) {
-	return nextScore < min ? nextScore : min;
+const midTermScores = [ 70, 88, 93, 94, 64, 62, 56 ];
+const finalScores = [ 92, 93, 76, 77, 78, 59, 61 ];
+const minMidtermScore = midTermScores.reduce(function(accum, nextScore) {
+	if (accum < nextScore) {
+		return accum;
+	} else {
+		return nextScore;
+	}
 });
-const maxScore = midtermScores.reduce(function(max, nextScore) {
-	return nextScore > max ? nextScore : max;
-});
-
-// const minFinalsScore = finalsScores.reduce(function(min, nextScore) {
-// 	return nextScore < min ? nextScore : min;
-// });
-const minOverallScore = finalsScores.reduce(function(min, nextScore) {
+const minOverallScore = finalScores.reduce(function(min, nextScore) {
 	return nextScore < min ? nextScore : min;
 }, minMidtermScore);
